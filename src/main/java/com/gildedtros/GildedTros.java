0ca17f48ddbe401f89589f0c74bd769976170997
Mark <markpasterkamp@hotmail.com>
2024-03-18 13:50:38 +0100

    Refactor code by unwrapping if-statements

--- src/main/java/com/gildedtros/GildedTros.java
+++ src/main/java/com/gildedtros/GildedTros.java
@@ -12,56 +12,54 @@ class GildedTros {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(GOOD_WINE)
-                    && !items[i].name.equals(BACKSTAGE_PASSES_FOR_RE_FACTOR)
-                    && !items[i].name.equals(BACKSTAGE_PASSES_FOR_HAXX))
-            {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(B_DAWG_KEYCHAIN)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals(BACKSTAGE_PASSES_FOR_RE_FACTOR) || items[i].name.equals(BACKSTAGE_PASSES_FOR_HAXX) ) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+        for (Item item : items) {
+            switch (item.name) {
+                case GOOD_WINE -> goodWine(item);
+                case BACKSTAGE_PASSES_FOR_HAXX, BACKSTAGE_PASSES_FOR_RE_FACTOR -> backstagePass(item);
+                case B_DAWG_KEYCHAIN -> keychain(item);
+                default -> other(item);
             }
+        }
+    }
 
-            if (!items[i].name.equals(B_DAWG_KEYCHAIN)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+    private void goodWine(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality++;
+        }
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(GOOD_WINE)) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES_FOR_RE_FACTOR) && !items[i].name.equals(BACKSTAGE_PASSES_FOR_HAXX)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(B_DAWG_KEYCHAIN)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+    private void backstagePass(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+        if (item.sellIn < 11 && item.quality < 50) {
+            item.quality++;
+        }
+        if (item.sellIn < 6 && item.quality < 50) {
+            item.quality++;
+        }
+        item.sellIn--;
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void keychain(Item item) {
+
+    }
+
+    private void other(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+        item.sellIn--;
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality--;
             }
         }
     }
