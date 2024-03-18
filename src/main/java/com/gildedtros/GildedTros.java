d699ea2d817f861a903a33587b91055fd3ae281a
Mark <markpasterkamp@hotmail.com>
2024-03-18 10:21:48 +0100

    Extract all Strings to constants

--- src/main/java/com/gildedtros/GildedTros.java
+++ src/main/java/com/gildedtros/GildedTros.java
@@ -1,6 +1,10 @@
 package com.gildedtros;
 
 class GildedTros {
+    public static final String GOOD_WINE = "Good Wine";
+    public static final String BACKSTAGE_PASSES_FOR_RE_FACTOR = "Backstage passes for Re:Factor";
+    public static final String BACKSTAGE_PASSES_FOR_HAXX = "Backstage passes for HAXX";
+    public static final String B_DAWG_KEYCHAIN = "B-DAWG Keychain";
     Item[] items;
 
     public GildedTros(Item[] items) {
@@ -9,12 +13,12 @@ class GildedTros {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Good Wine")
-                    && !items[i].name.equals("Backstage passes for Re:Factor")
-                    && !items[i].name.equals("Backstage passes for HAXX"))
+            if (!items[i].name.equals(GOOD_WINE)
+                    && !items[i].name.equals(BACKSTAGE_PASSES_FOR_RE_FACTOR)
+                    && !items[i].name.equals(BACKSTAGE_PASSES_FOR_HAXX))
             {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("B-DAWG Keychain")) {
+                    if (!items[i].name.equals(B_DAWG_KEYCHAIN)) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -22,7 +26,7 @@ class GildedTros {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes for Re:Factor") || items[i].name.equals("Backstage passes for HAXX") ) {
+                    if (items[i].name.equals(BACKSTAGE_PASSES_FOR_RE_FACTOR) || items[i].name.equals(BACKSTAGE_PASSES_FOR_HAXX) ) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -38,15 +42,15 @@ class GildedTros {
                 }
             }
 
-            if (!items[i].name.equals("B-DAWG Keychain")) {
+            if (!items[i].name.equals(B_DAWG_KEYCHAIN)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Good Wine")) {
-                    if (!items[i].name.equals("Backstage passes for Re:Factor") && !items[i].name.equals("Backstage passes for HAXX")) {
+                if (!items[i].name.equals(GOOD_WINE)) {
+                    if (!items[i].name.equals(BACKSTAGE_PASSES_FOR_RE_FACTOR) && !items[i].name.equals(BACKSTAGE_PASSES_FOR_HAXX)) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("B-DAWG Keychain")) {
+                            if (!items[i].name.equals(B_DAWG_KEYCHAIN)) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
